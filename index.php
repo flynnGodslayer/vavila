6664c822f8f0cdd91cff0d2b80924228de9df332
root <root@flynndebian>
2018-03-08 09:01:56 -0600

    commit message

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Examen PArcial</title>
+</head>
+
+<body>
+	<h3>Victor Hugo Avila Felipe</h3>
+	<h4>Licenciatura en Informática</h4>
+<ul>
+	<h4>Materias favoritas</h4>
+	<a href="https://es.wikipedia.org/wiki/auditoría_informática">Auditoría en informática</a>
+	<a href="https://es.wikipedia.org/wiki/Sistema_operativo">Sistemas operativos Multiusuarios</a>
+	<a href="https://es.wikipedia.org/wiki/Contabilidad">Contabilidad</a>
+	<a href="https://es.wikipedia.org/wiki/Informática">Informática I fundamentos</a>
+	<a href="https://es.wikipedia.org/wiki/Mercadotecnia">Mercadotecnia</a>
+</ul>
+<ul>
+	<h4>Materias no favoritas</h4>
+	<a href="https://es.wikipedia.org/wiki/Recursos_humanos">Recursos Humanos</a>
+	<a href="https://es.wikipedia.org/wiki/Organización">Entorno de las organizaciones</a>
+	<a href="https://es.wikipedia.org/wiki/Coaching">Coaching</a>
+	<a href="https://es.wikipedia.org/wiki/Base_de_datos">Bases de datos</a>
+	<a href="https://es.wikipedia.org/wiki/Derecho_informático">Derecho informático</a>
+</ul>
+	<a href="/comentario.php">Ir a comentario.php</a>
+</body>
+<footer>
+	<a href="https://www.unam.mx">unam.mx</a>
+	<a href="https://www.fca.unam.mx">fca.unam.mx</a>
+	<a href="/creditos.php">Creditos</a>
+</footer>
